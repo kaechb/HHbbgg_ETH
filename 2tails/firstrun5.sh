7eb44f2c275f590587ad619a48576211583f5985
Benno Kaech <benno.kaech@hotmail.com>
2019-04-01 10:35:29 +0200

    two tails

---
+++ 2tails/firstrun5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=test_gpu
+#SBATCH --nodes=1       # request to run job on single node
+#SBATCH --ntasks=10     # request 10 CPU's (t3gpu01: balance between CPU and GPU : 5CPU/1GPU)
+#SBATCH --gres=gpu:1    # request 2 GPU's on machine
+#SBATCH --time=80:00:00 # time limit of job (8 minutes
+$CUDA_VISIBLE_DEVICES
+python /t3home/kaechb/HHbbgg_ETH_devel/bregression/notebooks/train_ffwd_phoEnergy.py --inp-dir=/scratch/mdonega/ntuples_NN --inp-file=Ntup_10Nov_Photon_training_allvars.hd5 --loss HuberLossOneTail --loss_params {\"deltaL\":0.5} --epochs=100 --out-dir=/work/kaechb/HuberLossOneTail5
+#ipython -i /t3home/kaechb/HHbbgg_ETH_devel/bregression/notebooks/predict_fit_ffwd_phoEnergy.py -- --training HuberLoss --inp-dir=/scratch/mdonega/ntuples_NN/ --inp-file=Ntup_10Nov_Photon_training_allvars.hd5 --target-dir=/work/kaechb/firstrunHuber --out-dir=/work/kaechb/firstrunHuber/
+
